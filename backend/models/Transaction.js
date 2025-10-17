1bb76892999a1cb4ebce4f79faf4dc093df37212
nyggebygge <innebandy2121@gmail.com>
2025-10-17 10:10:09 +0200

    new fix

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { sequelize } = require('../config/database');
 
 const Transaction = sequelize.define('Transaction', {
   id: {
